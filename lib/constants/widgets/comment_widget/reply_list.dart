646b8dc69fcaab45ead4dac83800e5b60a21f613
LeeYuCHhul <yoochul89@naver.com>
2024-04-03 17:37:58 +0900

    0403_질문하기 API 모두 연결 완료

--- lib/constants/widgets/comment_widget/reply_list.dart
+++ lib/constants/widgets/comment_widget/reply_list.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:starting_block/constants/constants.dart';
 
-class ReplyList extends StatelessWidget {
+class ReplyList extends StatefulWidget {
   final String thisUserName, thisDate, thisAnswer;
   final int thisLike;
   final bool isMyHeart;
+  final VoidCallback thisReplyHeartTap, thisReplyHeartDeleteTap;
 
   const ReplyList({
     super.key,
@@ -13,11 +14,18 @@ class ReplyList extends StatelessWidget {
     required this.thisAnswer,
     required this.thisLike,
     required this.isMyHeart,
+    required this.thisReplyHeartTap,
+    required this.thisReplyHeartDeleteTap,
   });
 
   @override
+  State<ReplyList> createState() => _ReplyListState();
+}
+
+class _ReplyListState extends State<ReplyList> {
+  @override
   Widget build(BuildContext context) {
-    String formattedDate = formatDate(thisDate);
+    String formattedDate = formatDate(widget.thisDate);
 
     return SizedBox(
       width: MediaQuery.of(context).size.width,
@@ -33,7 +41,7 @@ class ReplyList extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                thisUserName,
+                widget.thisUserName,
                 style: AppTextStyles.bd5.copyWith(color: AppColors.g6),
               ),
               Text(
@@ -42,33 +50,38 @@ class ReplyList extends StatelessWidget {
               ),
               Gaps.v4,
               Text(
-                thisAnswer,
+                widget.thisAnswer,
                 style: AppTextStyles.bd2.copyWith(color: AppColors.g6),
               ),
               Gaps.v4,
               Ink(
                 height: 26,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    isMyHeart
-                        ? AppIcon.vote_active_18
-                        : AppIcon.vote_inactive_18,
-                    Gaps.h2,
-                    Text(
-                      '도움',
-                      style: isMyHeart
-                          ? AppTextStyles.btn2.copyWith(color: AppColors.blue)
-                          : AppTextStyles.btn2.copyWith(color: AppColors.g4),
-                    ),
-                    Gaps.h2,
-                    Text(
-                      thisLike.toString(),
-                      style: isMyHeart
-                          ? AppTextStyles.btn2.copyWith(color: AppColors.blue)
-                          : AppTextStyles.btn2.copyWith(color: AppColors.g4),
-                    ),
-                  ],
+                child: InkWell(
+                  onTap: widget.isMyHeart
+                      ? widget.thisReplyHeartDeleteTap
+                      : widget.thisReplyHeartTap,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      widget.isMyHeart
+                          ? AppIcon.vote_active_18
+                          : AppIcon.vote_inactive_18,
+                      Gaps.h2,
+                      Text(
+                        '도움',
+                        style: widget.isMyHeart
+                            ? AppTextStyles.btn2.copyWith(color: AppColors.blue)
+                            : AppTextStyles.btn2.copyWith(color: AppColors.g4),
+                      ),
+                      Gaps.h2,
+                      Text(
+                        widget.thisLike.toString(),
+                        style: widget.isMyHeart
+                            ? AppTextStyles.btn2.copyWith(color: AppColors.blue)
+                            : AppTextStyles.btn2.copyWith(color: AppColors.g4),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
