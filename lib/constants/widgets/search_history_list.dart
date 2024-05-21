8bc4bdb2cea89be2586d5ceb0c15d78dbcfa0571
LeeYuCHhul <yoochul89@naver.com>
2024-05-21 16:43:05 +0900

    0521_광고 ID 허용 수정

--- lib/constants/widgets/search_history_list.dart
+++ lib/constants/widgets/search_history_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:starting_block/constants/constants.dart';
 
 class SearchHistoryList extends StatelessWidget {
